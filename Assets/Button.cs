03fd2b7a4ee2ba4631b9067ecb9767b1ee1f493b
Atarico <luiz_paulo.09@hotmail.com>
2015-12-07 16:50:18 -0300

    fase 3 e esboço da fase 4
    pls god don't bug this shit, i luv u

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Button : MonoBehaviour {
 
 	public GameObject target;
+	public Animator targetAnimator;
 
 	private SpriteRenderer spriteRenderer;
 
@@ -15,23 +16,42 @@ public class Button : MonoBehaviour {
 	void Awake() {
 
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		targetAnimator = target.GetComponent<Animator>();
 	}
 	void OnTriggerEnter2D (Collider2D col) {
 
 		if ((col.gameObject.tag == "Player" || col.gameObject.tag == "Box" || col.gameObject.tag == "Player2") && quemSubiu == "") {
 
-			target.SetActive(false);
-			spriteRenderer.sprite = pressedButton; 
-
+			Acction(1);
+			spriteRenderer.sprite = pressedButton;
 			quemSubiu = col.gameObject.tag;
 		}
 	}
 
 	void OnTriggerExit2D (Collider2D col) {
 		if (col.gameObject.tag == quemSubiu) {
-			target.SetActive (true);
+			Acction(2);
 			spriteRenderer.sprite = nonPressedButton;
 			quemSubiu = "";
+		}
+	}
+
+	void Acction (int key) {
+		if(target.gameObject.tag == "porta" && key == 1) {
+			//faz açao numero 1 da porta, por enquanto desativar
+			target.SetActive(false);
+		}
+		if(target.gameObject.tag == "porta" && key == 2) {
+			//faz açao numero 1 da porta, por enquanto desativar
+			target.SetActive(true);
+		}
+		if(target.gameObject.tag == "mola" && key == 1) {
+			//faz açao numero 1 da mola que e subir
+			targetAnimator.SetBool("subindo", true);
+		}
+		if(target.gameObject.tag == "mola" && key == 2) {
+			//faz açao numero 2 da mola que e descer
+			targetAnimator.SetBool("descendo", true);
 		}
 	}
 }
